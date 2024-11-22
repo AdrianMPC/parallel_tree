1ed3525124ae682e3427868fc1cc11384dc8159d
Adrian <adrian.pinado@usil.pe>
2024-11-22 09:47:52 -0500

    correccion

--- sensores/parallel_tree.cpp
+++ sensores/parallel_tree.cpp
@@ -24,10 +24,22 @@ double ParallelTree::calculateMaxAverageInternal(SensorTree* node_ptr) {
   double current_avg = 0.0;
   if(cont > 0) current_avg = sum / (double)cont;
 
-  // llamadas recursivas para los hijos
-  double max_avg_left = calculateMaxAverageInternal(node_ptr->left);
-  double max_avg_right = calculateMaxAverageInternal(node_ptr->right);
+  double max_avg_left = 0.0;
+  double max_avg_right = 0.0;
 
+  // llamadas recursivas para los hijos
+  #pragma omp parallel sections
+  {
+    #pragma omp section
+    {
+      max_avg_left = calculateMaxAverageInternal(node_ptr->left);
+    }
+
+    #pragma omp section
+    {
+      max_avg_right = calculateMaxAverageInternal(node_ptr->right);
+    }
+  }
   // retornamos el máximo del promedio del nodo y sus hijos
   return std::max(std::max(current_avg, max_avg_left), max_avg_right);
 }
